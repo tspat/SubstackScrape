bbbedd20528eb1012293cbb7c28dd8e8d88d0902
tspat <scott@tumble.to>
2020-07-19 14:51:10 -0700

    changed import to require

--- scrape.js
+++ scrape.js
@@ -1,5 +1,5 @@
-import Axios from "axios"
-import Cheerio from "cheerio"
+const axios = require('axios');
+const cheerio = require('cheerio')
 
 
 const url = 'https://scottpatterson.substack.com/p/string-of-beginnings';
@@ -8,10 +8,10 @@ let postContents = [];
     
 function scrape() {
 
-    Axios.get("https://scottpatterson.substack.com/p/string-of-beginnings")
+    axios.get("https://scottpatterson.substack.com/p/string-of-beginnings")
         .then(response => {
             const html = response.data;
-            const $ = Cheerio.load(html);
+            const $ = cheerio.load(html);
             const post = $('div .body ');
             
 
